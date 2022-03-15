f6d1934bf622dfc73010efac043f934fe0b9c595
steven <steven.nevins.js@gmail.com>
2022-03-15 08:07:03 -0400

    add neoscroll for smoother scrolling in nvim

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ end
 
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- Package manager
+  use 'karb94/neoscroll.nvim'
   use 'prettier/vim-prettier'
   use 'tpope/vim-projectionist'
   use 'Mofiqul/vscode.nvim'
@@ -68,6 +69,26 @@ vim.cmd [[
     autocmd TextYankPost * silent! lua vim.highlight.on_yank()
   augroup end
 ]]
+-- neoscroll 
+
+require('neoscroll').setup({
+    -- Set any options as needed
+})
+
+local t = {}
+
+-- Syntax: t[keys] = {function, {function arguments}}
+t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '250'}}
+t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '250'}}
+t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '450'}}
+t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '450'}}
+t['<C-y>'] = {'scroll', {'-0.10', 'false', '100'}}
+t['<C-e>'] = {'scroll', { '0.10', 'false', '100'}}
+t['zt']    = {'zt', {'250'}}
+t['zz']    = {'zz', {'250'}}
+t['zb']    = {'zb', {'250'}}
+
+require('neoscroll.config').set_mappings(t)
 -- prettier on save
 vim.cmd [[
 autocmd BufWritePre * Prettier

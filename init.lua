b31afae5fcae5edafdc20fed3847ed484a06b47b
steven <steven.nevins.js@gmail.com>
2022-03-15 10:08:19 -0400

    smaller gutter

--- init.lua
+++ init.lua
@@ -36,7 +36,7 @@ require('packer').startup(function(use)
 vim.o.expandtab = true                                                                                                                                                   
 vim.o.shiftwidth = 4                                                                                                                                                     
 vim.o.tabstop = 4                                                                                                                                                        
-vim.o.signcolumn = 'yes:2'                                                                                                                                               
+vim.o.signcolumn = 'yes:1'                                                                                                                                               
 vim.o.relativenumber = true                                                                                                                                              
 vim.o.number = true                                                                                                                                                      
 vim.o.termguicolors = true                                                                                                                                               

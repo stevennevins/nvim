0e3746631f09ad91199013f45acb8d3784d2394a
steven nevins <steven.nevins.js@gmail.com>
2022-10-27 10:56:34 -0400

    ✏️ fix cr typo

--- init.lua
+++ init.lua
@@ -108,7 +108,7 @@ require('gitsigns').setup {
         -- Actions
         map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
         map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-        map({'n', 'v'}, '<leader>gp', ':term git pushCR>')
+        map({'n', 'v'}, '<leader>gp', ':term git push<CR>')
         map('n', '<leader>hS', gs.stage_buffer)
         map('n', '<leader>hu', gs.undo_stage_hunk)
         map('n', '<leader>hR', gs.reset_buffer)

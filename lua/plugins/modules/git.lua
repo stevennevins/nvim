59702a423faf8de46ee6105aa2dacfafcb038393
steven nevins <steven.nevins.js@gmail.com>
2023-09-25 16:51:36 -0400

    fix

--- lua/plugins/modules/git.lua
+++ lua/plugins/modules/git.lua
@@ -5,7 +5,7 @@ return {
     {
         "lewis6991/gitsigns.nvim",
         config = function()
-            require("gitsigns").setup()
+            require("gitsigns").setup({})
         end,
     },
 }

1648925ee7340288e0c28640ee86bdc4ca872cf5
steven nevins <steven.nevins.js@gmail.com>
2023-09-25 14:37:33 -0400

    remove autocmd

--- lua/config/augroup.lua
+++ lua/config/augroup.lua
@@ -9,19 +9,3 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
     end,
 })
-
--- svelte hack to restart lsp on file save
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("svelte_restart_lsp_on_save", { clear = true }),
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        vim.api.nvim_create_autocmd("BufWritePost", {
-            pattern = { "*.js", "*.ts" },
-            callback = function()
-                if client.name == "svelte" then
-                    vim.cmd("LspRestart")
-                end
-            end,
-        })
-    end,
-})

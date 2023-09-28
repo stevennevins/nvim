e54c9b840eab98d735bc41c1d3d9ea0b6f1b179c
steven nevins <steven.nevins.js@gmail.com>
2023-09-28 17:39:04 -0400

    update config and add ascii to tree diagram

--- lua/plugins/modules/telescope.lua
+++ lua/plugins/modules/telescope.lua
@@ -3,6 +3,22 @@ local function map(mode, l, r, opts)
     opts.buffer = bufnr
     vim.keymap.set(mode, l, r, opts)
 end
+
+local builtin = require("telescope.builtin")
+map("n", "<leader>d", builtin.diagnostics)
+map("n", "<leader>r", builtin.lsp_references)
+map("n", "<leader>t", builtin.lsp_type_definitions)
+map("n", "<leader>i", builtin.lsp_implementations)
+map("n", "<leader>b", builtin.buffers)
+map("n", "<leader>f", builtin.git_files)
+map("n", "<leader>f", function()
+    local ok = pcall(require("telescope.builtin").git_files)
+    if not ok then
+        require("telescope.builtin").find_files()
+    end
+end)
+map("n", "<leader>g", builtin.live_grep)
+map("n", "<leader>p", builtin.resume)
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
@@ -12,7 +28,6 @@ return {
         local telescope = require("telescope")
 
         local telescopeConfig = require("telescope.config")
-        local builtin = require("telescope.builtin")
         -- Clone the default Telescope configuration
         local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
 
@@ -32,14 +47,6 @@ return {
             },
         })
 
-        map("n", "<leader>d", builtin.diagnostics)
-        map("n", "<leader>r", builtin.lsp_references)
-        map("n", "<leader>t", builtin.lsp_type_definitions)
-        map("n", "<leader>i", builtin.lsp_implementations)
-        map("n", "<leader>b", builtin.buffers)
-        map("n", "<leader>f", builtin.git_files)
-        map("n", "<leader>g", builtin.live_grep)
-        map("n", "<leader>p", builtin.resume)
         telescope.load_extension("fzy_native")
     end,
 }

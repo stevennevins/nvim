cf9418c82c6d84439241ce4395964c027a6ddd76
steven nevins <steven.nevins.js@gmail.com>
2023-12-04 09:53:16 -0500

    update lsp and chatgpt plugins

--- lua/plugins/modules/ai.lua
+++ lua/plugins/modules/ai.lua
@@ -1,20 +1,23 @@
 return {
-    {
-        "David-Kunz/gen.nvim",
-        event = "VeryLazy",
-    },
+    -- {
+    --     "David-Kunz/gen.nvim",
+    --     event = "VeryLazy",
+    --     config = function()
+    --         require("gen").model = "codellama"
+    --     end,
+    -- },
     {
         "james1236/backseat.nvim",
         event = "VeryLazy",
         opts = {
-            openai_model_id = "gpt-3.5-turbo-16k",
+            openai_model_id = "gpt-4-1106-preview",
         },
     },
     {
         "CoderCookE/vim-chatgpt",
         event = "VeryLazy",
         config = function()
-            vim.g.chat_gpt_mod = "gpt-4-32k"
+            vim.g.chat_gpt_model = "gpt-4-1106-preview"
         end,
     },
 }
